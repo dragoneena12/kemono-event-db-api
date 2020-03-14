f2d70344bea2e17fbc995f88fa61fd0c9341333e
lapi <tyoutugai.7@gmail.com>
2020-03-14 17:56:29 +0900

    CORS

--- app/settings.py
+++ app/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,3 +145,11 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'lhp.Account'
+
+# 許可するオリジン
+CORS_ORIGIN_WHITELIST = [
+    'https://www.lapi.gq',
+    'http://localhost:3000',
+]
+# レスポンスを公開する
+CORS_ALLOW_CREDENTIALS = True
